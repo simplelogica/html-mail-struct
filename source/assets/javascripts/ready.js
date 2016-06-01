10f1c14f2d01cd48feb795cf5ba23259031db0a8
carloscabo <carlos.cabo@the-cocktail.com>
2016-06-01 12:59:30 +0200

    Añadido data-attribute en el layout general para setear el height de los iframes

--- source/assets/javascripts/ready.js
+++ source/assets/javascripts/ready.js
@@ -1,4 +1,11 @@
 $(function() {
   // La magia aquí
   console.log('Ready to go!');
+
+  $('[data-iframe-h]').each( function( idx, el ) {
+    var
+      $el = $(el),
+      h = $el.attr('data-iframe-h')+'px';
+    $el.find('iframe').css('height', h);
+  });
 });
